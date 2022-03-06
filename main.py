426ed089200712b20dcf90cb0ebc1be643de4cfe
David Martinez <>
2022-03-06 10:10:46 +0000

    Backend changes

--- main.py
+++ main.py
@@ -1,15 +1,47 @@
 from flask import Flask, render_template
 from replit import web
-
+import lob
 app = Flask(__name__)
+lob.api_key = 'secretapikeythatiwilladdlater'
 
 @app.route('/')
 def index():
-  return 'Wassup'
+  return render_template("index.html")
 
 @app.route('/about')
 def about():
-  return 
+  return render_template("about.html")
 
-@app.route('/signin')
+@app.route('/info')
+def info(): 
+  return render_template("info.html")
+
+@app.route('/getinvolved')
+def getinvolved():
+  return render_template("getinvolved.html")
+  
+@app.route('/signup')
+def signup():
+  return render_template("signin.html")
+  
+def postcard(name, address_line1, address_line2, address_city, address_state, address_zip):
+  card = lob.Postcard.create(
+    to_address = {
+      'name': name,
+      'address_line1': address_line1,
+      'address_line2': address_line2,
+      'address_city': address_city,
+      'address_state': address_state,
+      'address_zip': address_zip
+    },
+  front = '<html style="padding: 1in; font-size: 50;">Front HTML for {{name} </html>',
+  back = '<html style="padding: 1in; font-size: 20;">Back HTML for {{name}}</html>',
+  ) # Hard code the jpgs into front and back. 
+  return card
+  
+def dispCard():
+  return lob.Postcard.list(limit=10)
+
+def get_name(): 
+        print(Hello what is your name)
 web.run(app)
